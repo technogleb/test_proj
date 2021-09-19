0e948444c54144d2dc16f5895888e2ae66a7852a
Gleb Sinyakov <sinyakov@phystech.edu>
2021-09-19 12:30:42 +0300

    Make initial flask structure (#1)
    * Make initial flask structure
    
    * Update port

---
+++ test_proj/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+
+@app.route('/index', strict_slashes=False)
+def index():
+    return 'app is working'
+
+
+if __name__ == "__main__":
+    app.run('localhost', 80)
